10f476bdda3515061aa18e93cf061c930d4f0268
Matas Kaminskas <matas.kaminskas@protonmail.com>
2023-10-08 11:00:53 +0300

    feat: button hidden content

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -4,6 +4,15 @@ const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
-window.Stimulus   = application
+window.Stimulus = application
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('showHideButton');
+    const contentToToggle = document.getElementById('contentToToggle');
+
+    button.addEventListener('click', function () {
+        contentToToggle.classList.toggle('hidden');
+    });
+});
 
 export { application }
